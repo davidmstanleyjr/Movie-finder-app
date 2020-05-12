6352058936bb62ef786c33bc5b2630dd63cf710b
David Stanley <davidmstanleyjr@gmail.com>
2020-05-12 00:33:19 -0400

    starting main.js. Wrote search and submit logic

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//This is for searching and submitting. The preventDefault keeps the page from refreshing
+
+$(document).ready(() => {
+	$('#searchForm').on('submit', (e) => {
+		let searchText = $('#searchText').val();
+		getMovies(searchText);
+		e.preventDefault();
+	});
+});
+
+function getMovies(searchText) {
+	console.log(searchText);
+}
