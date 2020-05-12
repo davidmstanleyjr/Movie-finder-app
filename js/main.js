09a302765018b4e6eea135a338f440e4e0a6db57
David Stanley <davidmstanleyjr@gmail.com>
2020-05-12 16:46:14 -0400

    films and posters now populate the page, as well as the link to movie details

--- js/main.js
+++ js/main.js
@@ -14,6 +14,21 @@ function getMovies(searchText) {
 		.get(' http://www.omdbapi.com/?s=' + searchText + '&apikey=72455408&')
 		.then((response) => {
 			console.log(response);
+			let movies = response.data.Search;
+			let output = '';
+			$.each(movies, (index, movie) => {
+				output += `
+                <div class="col-md-3">
+                <div class="well text-center"> 
+                    <img src="${movie.Poster}">             
+                    <h5>${movie.Title}</h5>
+                    <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+                </div>
+                </div>
+                `;
+			});
+
+			$('#movies').html(output);
 		})
 		.catch((err) => {
 			console.log(err);
