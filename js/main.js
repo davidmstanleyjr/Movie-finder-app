15f15df971f016c20f748dc24b8963b6b62dd4c7
David Stanley <davidmstanleyjr@gmail.com>
2020-05-13 17:15:08 -0400

    still trying to figure it out.

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(document).ready(() => {
 //Function for searching for movies. Catch error is in case something screws up.
 function getMovies(searchText) {
 	axios
-		.get(' http://www.omdbapi.com/?s=' + searchText + '&apikey=72455408&')
+		.get(' https://www.omdbapi.com/?s=' + searchText + '&apikey=72455408&')
 		.then((response) => {
 			console.log(response);
 			let movies = response.data.Search;
@@ -44,7 +44,7 @@ function movieSelected(id) {
 function getMovie() {
 	let movieId = sessionStorage.getItem('movieId');
 	axios
-		.get('https://www.omdbapi.com/?i=' + movieId + '&apikey=72455408&')
+		.get(' https://www.omdbapi.com/?i=' + movieId + '&apikey=72455408&')
 		.then((response) => {
 			console.log(response);
 			let movie = response.data;
@@ -75,7 +75,7 @@ function getMovie() {
 
 					${movie.Plot}
 					<hr>
-					<a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
+					<a href="https://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
 					<a href="index.html" class="btn btn-default">Go Back to Search</a>
 
 				</div>
