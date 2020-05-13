2a5e20d8914094d3519753a5d5b566edeeae758b
David Stanley <davidmstanleyjr@gmail.com>
2020-05-13 15:14:35 -0400

    can now click on movie details and it takes you to movie.html. All it shows is a poster tho. Will add more soon.

--- js/main.js
+++ js/main.js
@@ -34,3 +34,35 @@ function getMovies(searchText) {
 			console.log(err);
 		});
 }
+
+function movieSelected(id) {
+	sessionStorage.setItem('movieId', id);
+	window.location = 'movie.html';
+	return false;
+}
+
+function getMovie() {
+	let movieId = sessionStorage.getItem('movieId');
+	axios
+		.get(' http://www.omdbapi.com/?i=' + movieId + '&apikey=72455408&')
+		.then((response) => {
+			console.log(response);
+			let movie = response.data;
+
+			let output = `
+			<div class="row">
+				<div class="col-md-4">
+					<img src="${movie.Poster}" class="thumbnail">
+			</div>
+			<div class="col-md-8>"
+			
+				</div>
+			</div>
+			`;
+
+			$('#movie').html(output);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+}
